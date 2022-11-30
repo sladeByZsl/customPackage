323b6e506432f2616c9f1fd56ed1f583b10c740d
zsl <zhousilai@elex-tech.com>
2022-11-30 22:22:08 +0800

    111

---
+++ Editor/MonoEditor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MonoEditor : Editor
+{
+    // Start is called before the first frame update
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
---
+++ Runtime/MonoTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError("123");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
